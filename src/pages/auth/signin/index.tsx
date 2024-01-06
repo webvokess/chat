ed248c4245123098d49db50dc4525dc471036786
harshtrivs@gmail.com <Harshtrivs@gmail.com>
2024-01-06 15:20:21 +0530

    login page

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
 
-
-
 const Signin = () => {
+    
     return (
-        <div className='bg-dark ml-10'>
-            signin
-            <button className="btn btn-primary">Button</button>
+    <>
+    {/* <img src='/assets/loginPage.png' className="img-fluid" alt="..."/> */}
+    <div className='loginDiv container-fluid p-5'>
+        <div className="conatiner fluid bg-light border rounded-5 shadow-lg p-3 mb-5 bg-body-dark"  style={{width:'40vw', height:'80vh', position:'absolute',top:'10vh',right:'15vh'}}>
+                <div className=" mt-4 row justify-content-center mx-auto fw-bolder "style={{fontSize:'54px'}} > Sign in</div>
+            
+            <div className='m-2 p-5'>
+            <form>
+                    <div className="mb-3">
+                        <label  className="form-label fs-3 fw-semibold">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <div id="emailHelp" className="form-text fs-5">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label  className="form-label fs-3 fw-semibold">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                    
+                    <div className='row justify-content-center m-5 '>
+                    <button type="submit" className="btn btn-primary fs-5 ">Submit</button>
+                    </div>
+            </form>
+            <div className=''>
+            <hr className='mt-4'/>
+            </div>
+            </div>
         </div>
+    </div>
+    </>
     )
 }
 
